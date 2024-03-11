5ab672f8fee18c27649c8c8a4d04b4df0d675d4b
JoSungHyeon <jsh950618@gmail.com>
2024-03-11 15:14:19 +0900

    counter commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import './App.css'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  }
+  
   return (
     <div className='App'>
       <h1>Simple Counter</h1>
       <section>
-        <Viewer />
+        <Viewer count={count} />
       </section>
       <section>
-        <Controller />
+        <Controller onClickButton={onClickButton}/>
       </section>
     </div>
   )
